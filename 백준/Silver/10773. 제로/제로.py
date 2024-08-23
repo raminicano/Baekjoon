d8ae4f4eaced86701820e2904dd201ac03a64db4
송윤주 <97875918+raminicano@users.noreply.github.com>
2024-08-23 13:34:07 +0900

    [Silver IV] Title: 제로, Time: 2388 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,11 @@
+K = int(input())
+num_list = []
+
+for i in range (K):
+    num = int(input())
+    if num == 0:
+        num_list.pop()
+    else:
+        num_list.append(num)
+
+print(sum(num_list))
